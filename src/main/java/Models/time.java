b9be50bf699a4a83db31ed9208898b23769609d5
ONE BY ONE <mazen1998m@lice.com>
2022-01-25 00:15:59 +0200

    a

--- src/main/java/Models/time.java
+++ src/main/java/Models/time.java
@@ -4,7 +4,7 @@ public class time {
     LocalDateTime order_time;
     LocalDateTime Agreed_delivery_time;
     LocalDateTime Actual_delivery_time;
-    LocalDateTime delay_time;
+    LocalDateTime delay_time;//ghg
    /* String getOrderTimeFromDatabase(user localUser){
         convertLocalDateTimeToString(localUser.Time.)
         //go to the database and get order time
